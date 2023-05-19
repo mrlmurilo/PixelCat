4a918fa094eef56993d740f6276e88a5398485b1
M-Eleoterio <m.eleoteriovieira@gmail.com>
2023-05-19 16:27:04 -0300

    Query funcionando corretamente

--- tela_cadastre-se/cadastro.js
+++ tela_cadastre-se/cadastro.js
@@ -1,20 +1,25 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'AWS',
+    host: 'database-1.cxcpz2ybu3tg.sa-east-1.rds.amazonaws.com',
     user: 'admin',
     password: 'PixelCats',
     database: 'pixelcats'
 });
 
-connection.connect( function (err){
-    console.log("Conexão Realizada com sucesso!");
-});
-
-connection.query("SELECT nome_cliente FROM cliente", function (err, rows, fields){
-    if(!err){
-        console.log("Resultado:", rows);
-    }else{
-        console.log('Erro: Consulta não realizada');
+connection.connect(function(err) {
+    if (err) {
+        console.error('Erro ao conectar ao MySQL:', err);
+        return;
     }
+    console.log('Conexão realizada com sucesso!');
+
+    connection.query("SELECT nome_cliente FROM cliente", function (err, rows, fields){
+        if(!err){
+            console.log("Resultado:", rows);
+        }else{
+            console.log(err);
+        }
+        connection.end(); // Encerra a conexão após executar a consulta
+    });
 });
