f3a383431d8b7bfbe41b83f413149f1f5d326f34
M-Eleoterio <m.eleoterio@gmail.com>
2023-05-26 17:16:41 -0300

    .

--- tela_cadastre-se/cadastro.js
+++ tela_cadastre-se/cadastro.js
@@ -20,48 +20,20 @@ connection.connect((err) => {
 
 // Configuração do body-parser para receber dados do formulário
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 // Rota para exibir o formulário de cadastro
 app.get('/', (req, res) => {
-  res.send(`
-  <!DOCTYPE html>
-<html lang="en">
+ // Lê o conteúdo do arquivo HTML externo
+ fs.readFile('tela_cadastre-se/cadastro.html', 'utf8', (err, htmlContent) => {
+  if (err) {
+    console.error('Erro ao ler o arquivo HTML:', err);
+    return res.status(500).send('Erro ao processar a requisição');
+  }
 
-<head>
-    <link rel="stylesheet" href="Style.css">
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <h1>Cadastre<span id="cor2">-se</span></h1>
-    <div class="painel-de-cadastro">
-        <form id="formCad" action="/formulario" method="POST">
-            <p>Nome:</p>
-            <input class="inserir-texto" type="text" name="nome_cliente" id="nome_cliente" required>
-            <p>Email:</p>
-            <input class="inserir-texto" type="text" name="email_cliente" id="email_cliente" required>
-            <p>CPF:</p>
-            <input class="inserir-texto" type="text" name="cpf_cliente" id="cpf_cliente" required>
-            <p>Data de Nascimento:</p>
-            <input class="inserir-texto" type="text" name="dataNasc_cliente" id="dataNasc_cliente" required>
-            <p>Telefone:</p>
-            <input class="inserir-texto" type="text" name="telefone_cliente" id="telefone_cliente" required>
-            <p>Endereço:</p>
-            <input class="inserir-texto" type="text" name="endereco_cliente" id="endereco_cliente" required>
-            <br><br>
-            <input type="submit" id="btnCad" value="Cadastrar"></input>
-        </form>
-    </div>
-
-    <script src="tela_cadastre-se/cadastro.js"></script>
-</body>
-
-</html>
-
-  `);
+  // Envia o conteúdo do arquivo HTML como resposta
+  res.send(htmlContent);
+});
 });
 
 // Rota para cadastrar os dados no MySQL
@@ -75,6 +47,6 @@ app.post('/formulario', (req, res) => {
 });
 
 // Inicia o servidor
-app.listen(3001, () => {
-  console.log('Servidor iniciado na porta 3001');
+app.listen(3002, () => {
+  console.log('Servidor iniciado na porta 3002');
 });
