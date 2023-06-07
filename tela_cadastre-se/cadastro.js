858086fb06b50fcef4ef2a7ddbdbd3f31eef4c46
M-Eleoterio <m.eleoteriovieira@gmail.com>
2023-06-06 23:41:39 -0300

    tentativa de fazer as imagens do login carregarem

--- tela_cadastre-se/cadastro.js
+++ tela_cadastre-se/cadastro.js
@@ -11,10 +11,10 @@ app.use(bodyParser.json());
 
 // Configurações do MySQL
 const connection = mysql.createConnection({
-  host: 'database-1.cxcpz2ybu3tg.sa-east-1.rds.amazonaws.com',
-  user: 'admin',
-  password: 'PixelCats',
-  database: 'pixelcats'
+  host: 'localhost',
+  user: 'root',
+  password: 'Eleoterio2327!',
+  database: 'pixelcat'
 });
 
 app.use(express.static(path.join(__dirname, 'tela_cadastre-se')));
@@ -25,26 +25,25 @@ app.get('/', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-  
+  res.redirect('http://localhost:2000');
 });
 
 
 // Rota para lidar com o envio do formulário de cadastro
 app.post('/formulario', (req, res) => {
   // Obtenha os dados do formulário enviados pelo cliente
-  const { nome_cliente, email_cliente, cpf_cliente, dataNasc_cliente, telefone_cliente, endereco_cliente } = req.body;
+  const { nome_cliente, email_cliente, senha_cliente, cpf_cliente, dataNasc_cliente, telefone_cliente, endereco_cliente } = req.body;
 
   // Construir a query SQL de inserção
-  const query = `INSERT INTO cliente (nome_cliente, email_cliente, cpf_cliente, dataNasc_cliente, telefone_cliente, endereco_cliente) VALUES (?, ?, ?, ?, ?, ?)`;
+  const query = `INSERT INTO cliente (nome_cliente, email_cliente, senha_cliente, cpf_cliente, dataNasc_cliente, telefone_cliente, endereco_cliente) VALUES (?, ?, ?, ?, ?, ?, ?)`;
 
   // Executar a query SQL para inserir os dados no banco de dados
-  connection.query(query, [nome_cliente, email_cliente, cpf_cliente, dataNasc_cliente, telefone_cliente, endereco_cliente], (err, results) => {
+  connection.query(query, [nome_cliente, email_cliente, senha_cliente, cpf_cliente, dataNasc_cliente, telefone_cliente, endereco_cliente], (err, results) => {
     if (err) {
       console.error('Erro ao inserir os dados:', err);
       res.status(500).send('Erro ao realizar o cadastro');
     } else {
       console.log('Dados inseridos com sucesso');
-      res.send('Cadastro realizado com sucesso!');
       res.redirect('/login');
       connection.end();
     }
--- tela_login/script.js
+++ tela_login/script.js
@@ -3,16 +3,20 @@ const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const port = 4000;
+const port = 2000;
+
+app.use(express.static(path.join(__dirname, 'img_login')));
+
 const connection = mysql.createConnection({
-  host: 'database-1.cxcpz2ybu3tg.sa-east-1.rds.amazonaws.com',
-  user: 'admin',
-  password: 'PixelCats',
-  database: 'pixelcats',
+  host: 'localhost',
+  user: 'root',
+  password: 'Eleoterio2327!',
+  database: 'pixelcat'
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 
 connection.connect((error) => {
@@ -23,32 +27,38 @@ connection.connect((error) => {
   console.log('Conexão com o banco de dados estabelecida com sucesso!');
 });
 
+
+//FIM DA PARTE DE CONEXAO
+
 app.get('/', (req, res) => {
-    const filePath = path.join(__dirname, 'login.html');
-    res.sendFile(filePath);
-  });
+  const filePath = path.join(__dirname, 'login.html');
+  res.sendFile(filePath);
+});
 
 app.post('/login', (req, res) => {
-    const {email, password} = req.body;
-  
-    const query = 'SELECT * FROM cliente WHERE email_cliente = ? AND senha_cliente = ?';
-    connection.query(query, [email, password], (error, results) => {
-      if (error) {
-        console.error('Erro ao executar a consulta: ', error);
-        res.status(500).send('Erro ao executar a consulta');
-        return;
-      }
-  
-      if (results.length === 0) {
-        res.status(401).send('Credenciais inválidas');
-        return;
-      }
-  
-      res.status(200).send('Login bem-sucedido');
-    });
+  const { email, password } = req.body;
+  const filePath = path.join(__dirname, 'login.html');
+
+  const query = 'SELECT * FROM cliente WHERE email_cliente = ? AND senha_cliente = ?';
+  connection.query(query, [email, password], (error, results) => {
+    if (error) {
+      console.error('Erro ao executar a consulta: ', error);
+      res.status(500).send('Erro ao executar a consulta');
+      res.sendFile(filePath);
+      return;
+    }
+
+    if (results.length === 0) {
+      res.status(401).send('Credenciais inválidas');
+      res.sendFile(filePath);
+      return;
+    }
+
+    res.status(200).send('Login bem-sucedido');
   });
-  
-  // Iniciar o servidor
+});
+
+// Iniciar o servidor
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
-  });
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
