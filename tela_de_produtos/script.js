29ee98dde4573854319b99bffed70ef443a507a5
Murilo Margonar <murilomargonar@gmail.com>
2023-06-15 11:40:00 -0300

    arrumando html e css

---
+++ tela_de_produtos/script.js
@@ -0,0 +1,18 @@
+var header = document.getElementById('top');
+var lastScrollPosition = 0;
+
+window.addEventListener('scroll', function () {
+    var currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+  
+    if (currentScrollPosition > lastScrollPosition) {
+      // Scroll para baixo
+      header.classList.add('small');
+      document.body.classList.add('scroll-down');
+    } else {
+      // Scroll para cima
+      header.classList.remove('small');
+      document.body.classList.remove('scroll-down');
+    }
+  
+    lastScrollPosition = currentScrollPosition;
+  });
