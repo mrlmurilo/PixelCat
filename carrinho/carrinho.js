feb86c50547129ab6ea1dea4bb5b7a39f5ea95d2
M-Eleoterio <m.eleoteriovieira@gmail.com>
2023-06-18 19:45:52 -0300

    criado tela de carrinho (nao responsivo/só visual)

--- carrinho/carrinho.js
+++ carrinho/carrinho.js
@@ -1,42 +1,18 @@
-// Dados de exemplo
-const products = [
-    { name: 'Produto 1', quantity: 2, price: 10 },
-    { name: 'Produto 2', quantity: 1, price: 20 },
-    { name: 'Produto 3', quantity: 3, price: 15 }
-  ];
-  
-  // Função para renderizar os produtos no carrinho
-  function renderProducts() {
-    const productUl = document.getElementById('product-list');
-    productUl.innerHTML = '';
-  
-    let total = 0;
-  
-    products.forEach((product) => {
-      const li = document.createElement('li');
-      li.textContent = `${product.name} - Quantidade: ${product.quantity}`;
-      productUl.appendChild(li);
-  
-      total += product.quantity * product.price;
-    });
-  
-    const totalValue = document.getElementById('total-value');
-    totalValue.textContent = `Valor Total: R$ ${total.toFixed(2)}`;
+var header = document.getElementById('top');
+var lastScrollPosition = 0;
+
+window.addEventListener('scroll', function () {
+  var currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (currentScrollPosition > lastScrollPosition) {
+    // Scroll para baixo
+    header.classList.add('small');
+    document.body.classList.add('scroll-down');
+  } else {
+    // Scroll para cima
+    header.classList.remove('small');
+    document.body.classList.remove('scroll-down');
   }
-  
-  // Função para atualizar a quantidade de um produto
-  function updateQuantity(productIndex, newQuantity) {
-    products[productIndex].quantity = newQuantity;
-    renderProducts();
-  }
-  
-  // Evento do botão "Finalizar Compra"
-  const checkoutButton = document.getElementById('checkout-button');
-  checkoutButton.addEventListener('click', () => {
-    // Lógica para finalizar a compra
-    alert('Compra finalizada com sucesso!');
-  });
-  
-  // Renderiza os produtos iniciais
-  renderProducts();
-  
+
+  lastScrollPosition = currentScrollPosition;
+});
